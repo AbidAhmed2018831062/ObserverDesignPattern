bb261e1256bdd4f9b0e292ef903886c296833c26
Abid <ahmedabid3409@gmail.com>
2022-03-30 02:58:27 +0600

    Youtube Updated

--- src/observer/Subscriber.java
+++ src/observer/Subscriber.java
@@ -2,12 +2,18 @@ package observer;
 
 public class Subscriber {
   String name;
+  Channel ch;
   Subscriber(String name)
   {
       this.name=name;
   }
+
+  void subChannel(Channel ch)
+  {
+this.ch=ch;
+  }
   void update(String channelName)
   {
-      System.out.println("Hey,"+channelName+" uploaded a new video");
+      System.out.println("Hey,"+this.name+" "+channelName+" uploaded a new video");
   }
 }
--- src/observer/Youtube.java
+++ src/observer/Youtube.java
@@ -3,6 +3,30 @@ package observer;
 public class Youtube {
     public static void main(String[]args)
     {
-        
+        Channel abid=new Channel("Learn with Abid");
+
+        Subscriber s1=new Subscriber("Abid");
+        Subscriber s2=new Subscriber("Shakib");
+        Subscriber s3=new Subscriber("Shrijon");
+        Subscriber s4=new Subscriber("Raju");
+        Subscriber s5=new Subscriber("Fahim");
+
+        s1.subChannel(abid);
+        s2.subChannel(abid);
+        s3.subChannel(abid);
+        s4.subChannel(abid);
+        s5.subChannel(abid);
+        s1.subChannel(abid);
+      
+        abid.addSubscriber(s1);
+        abid.addSubscriber(s2);
+        abid.addSubscriber(s3);
+        abid.addSubscriber(s4);
+        abid.addSubscriber(s5);
+
+        abid.notifySubscribers();
+
+
+
     }
 }
